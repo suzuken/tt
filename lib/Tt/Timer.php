b6689173c4ae6572972dbdaf943376051f8df2ba
suzuken <suzuken326@gmail.com>
2013-10-03 20:05:38 +0900

    odai1とodai2

--- lib/Tt/Timer.php
+++ lib/Tt/Timer.php
@@ -7,4 +7,18 @@ class Timer
     {
         return "hello world.";
     }
+
+    public function timeJapanize($time)
+    {
+        $t = \DateTime::createFromFormat("Y/m/d H:i:s", $time);
+        return $t->format("Y年m月d日 H時i分s秒");
+    }
+
+    public function timeDiff($from, $to)
+    {
+        $fromTime = \DateTime::createFromFormat("Y/m/d H:i:s", $from);
+        $toTime = \DateTime::createFromFormat("Y/m/d H:i:s", $to);
+        $interval = $fromTime->diff($toTime);
+        return $interval->format("%D") * 24 * 60+ $interval->format("%H") * 60 + $interval->format("%i");
+    }
 }
